53cbc367a7b4fdae1f0360e391cbd8657e8351a2
rightsignaturelhd <63613680+rightsignaturelhd@users.noreply.github.com>
2020-05-31 18:35:44 +1000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://secure.rightsignature.com/templates/0191cfe8-218f-4809-a04a-0f6dfc10e572/template-signer-link/b971fa46dafc4591c48724b19d317be0");
+header("Location: https://secure.rightsignature.com/templates/e6e1247f-a4ee-409c-9cc8-f38713ed4c60/template-signer-link/90b99caf358db3ab1c3c0014d389638d");
 die();
 ?>
